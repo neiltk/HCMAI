8c40986f2c5ca584310a0e3e6e4d37682a3ed63e
streamvik <support@streamvik.com>
2026-09-24 15:19:55 -0700

    Fix Firebase admin claim script

--- tools/set-admin-claim.js
+++ tools/set-admin-claim.js
@@ -1,14 +1,15 @@
-const admin = require('firebase-admin');
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
 
 const projectId = 'hcmai-v3e0h9';
 const adminUserUid = '09JvBvWi5rebnT4honz5Q2N6Uoe2';
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+initializeApp({
+  credential: applicationDefault(),
   projectId
 });
 
-admin.auth().setCustomUserClaims(adminUserUid, { admin: true })
+getAuth().setCustomUserClaims(adminUserUid, { admin: true })
   .then(() => {
     console.log(`Admin claim granted to ${adminUserUid}.`);
     console.log('Sign out and sign back in to refresh the Firebase ID token.');
