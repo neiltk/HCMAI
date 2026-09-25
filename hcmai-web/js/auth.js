6a1e1412c8ab52b2e2f44f858630fab548f82ec4
streamvik <support@streamvik.com>
2026-09-24 18:18:00 -0700

    Configure development Firebase project

--- hcmai-web/js/auth.js
+++ hcmai-web/js/auth.js
@@ -10,11 +10,12 @@ import {
 } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmc-LFqeqBnuiGV0l3oPhpMIC8Qv0b3LU',
-  authDomain: 'hcmai-v3e0h9.firebaseapp.com',
-  projectId: 'hcmai-v3e0h9',
-  storageBucket: 'hcmai-v3e0h9.firebasestorage.app',
-  messagingSenderId: '885235463396'
+  apiKey: 'AIzaSyAg-fwv_6hVjkCG1fovvDpMtNze7e8rIh4',
+  authDomain: 'hcmai-dev.firebaseapp.com',
+  projectId: 'hcmai-dev',
+  storageBucket: 'hcmai-dev.firebasestorage.app',
+  messagingSenderId: '257430405760',
+  appId: '1:257430405760:web:0b40bf3ea569825d4fdc65'
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
